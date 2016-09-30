f5ddb0c9afc07f6d64b74ca9c2274d049f2bd365
Radovan Bast <bast@users.noreply.github.com>
2016-09-30 17:52:12 +0200

    s/pbs/slurm/

--- development/files/perf-reports.sh
+++ development/files/perf-reports.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-#PBS -A_______
-#PBS -lnodes=1
-#PBS -lwalltime=00:10:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=0-00:10:00
 
 module load perf-reports/5.1
 
 # create temporary scratch area for this job on the global file system
-SCRATCH_DIRECTORY=/global/work/$USER/$PBS_JOBID
+SCRATCH_DIRECTORY=/global/work/$USER/$SLURM_JOBID
 mkdir -p $SCRATCH_DIRECTORY
 
 # run the performance report
 # all you need to do is to launch your "normal" execution
 # with "perf-report"
 cd $SCRATCH_DIRECTORY
-perf-report mpiexec -n $PBS_NP $PBS_O_WORKDIR/example.x
+perf-report mpiexec -n 20 $SLURM_SUBMIT_DIR/example.x
 
 # perf-report generates summary files in html and txt format
 # we copy result files to submit dir
-cp *.html *.txt $PBS_O_WORKDIR
+cp *.html *.txt $SLURM_SUBMIT_DIR
 
 # clean up the scratch directory
 cd /tmp
